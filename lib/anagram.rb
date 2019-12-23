b54a7a4549cb8979cff8f4b467b85dd5983c7382
Ayako Wiltshire <awittyl44@gmail.com>
2019-12-23 15:16:32 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+require 'pry'
+class Anagram
+  
+  attr_accessor :words
+  
+  def initialize(words)
+    
+    @words = words
+  end
+  
+  def detector(words)
+    words.each.tap {|word| word.spilt.sort}
+   binding.pry 
+    word
+     
+  end
+  
+  
+end
